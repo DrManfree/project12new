e8077bf1862cf37ce2e397009b3bfeb3013c626d
Alex <sponge.leha@gmail.com>
2021-10-24 02:47:11 +0300

    test commit

--- src/app/form-add/form-add.component.ts
+++ src/app/form-add/form-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-form-add',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormAddComponent implements OnInit {
 
-  constructor() { }
+  @Output() noteCreated = new EventEmitter<{ title: string, content: string, id: number, createdate: Date}>();
+  constructor() { 
+    num : Number;
+  }
 
   ngOnInit(): void {
   }
-
+  num = 0;
+  onAdd(noteNameInput : HTMLInputElement, noteContentInput: HTMLTextAreaElement) {
+    let crDate = new Date();
+    this.noteCreated.emit({
+      title: noteNameInput.value,
+      content: noteContentInput.value,
+      id: this.num,
+      createdate: crDate
+    });
+    this.num++;
+  }
 }
