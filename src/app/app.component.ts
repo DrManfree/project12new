d6aefb2a99acdb9250b90cbe0a1898bebb3ba34e
Alex <sponge.leha@gmail.com>
2021-10-24 02:50:52 +0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project12new';
+  cardElements: any = [];
+
+  onCardAdded(cardData: {title: string, content: string, id: number, createdate: Date}) {
+    this.cardElements.push({
+      title: cardData.title,
+      content: cardData.content,
+      id: cardData.id,
+      createdate: cardData.createdate
+    });
+  }
 }
