55ac7c6b94a78eeedcec4f89943256f9b7d8cf41
Alex <sponge.leha@gmail.com>
2021-10-24 05:26:53 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { CardModel } from './shared/models/card_model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,14 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project12new';
-  cardElements: any = [];
-
-  onCardAdded(cardData: {title: string, content: string, id: number, createdate: Date}) {
-    this.cardElements.push({
-      title: cardData.title,
-      content: cardData.content,
-      id: cardData.id,
-      createdate: cardData.createdate
+  cardElements : CardModel[] = [];
+  onCardAdded(cardData: {title: string, content: string, createdate: Date}) {
+    let temp : CardModel = new CardModel;
+    temp.title = cardData.title;
+    temp.content = cardData.content;
+    temp.id = this.cardElements.length + 1;
+    temp.createdate = cardData.createdate;
+    this.cardElements.push(temp);
+  }
+  onCardDeleted(card_index : { id: number })
+  {
+    this.cardElements.splice(card_index.id - 1, 1);
+    let num = 1;
+    this.cardElements.forEach(element => {
+      element.id = num;
+      num++;
     });
+  }
+  onCardEdited(info : {id: number, content: string, editdate: Date})
+  {
+    this.cardElements[info.id - 1].content = info.content;
+    this.cardElements[info.id - 1].createdate = info.editdate;
   }
 }
--- src/app/form-add/form-add.component.ts
+++ src/app/form-add/form-add.component.ts
@@ -7,22 +7,18 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class FormAddComponent implements OnInit {
 
-  @Output() noteCreated = new EventEmitter<{ title: string, content: string, id: number, createdate: Date}>();
+  @Output() noteCreated = new EventEmitter<{ title: string, content: string, createdate: Date}>();
   constructor() { 
-    num : Number;
   }
 
   ngOnInit(): void {
   }
-  num = 0;
   onAdd(noteNameInput : HTMLInputElement, noteContentInput: HTMLTextAreaElement) {
     let crDate = new Date();
     this.noteCreated.emit({
       title: noteNameInput.value,
       content: noteContentInput.value,
-      id: this.num,
       createdate: crDate
     });
-    this.num++;
   }
 }
